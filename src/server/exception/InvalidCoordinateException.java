57da9c7ab1f97974e242bc38ae12596f1ca3f036
mubashirtanveerayon <ayon261261skylinegtr@gmail.com>
2023-09-14 20:46:24 +0600

    del

--- src/server/exception/InvalidCoordinateException.java
+++
@@ -1,9 +0,0 @@
-package server.exception;
-
-public class InvalidCoordinateException extends RuntimeException{
-
-    public InvalidCoordinateException(int file,int rank){
-        super("Invalid coordinate: ["+file+","+rank+"]");
-    }
-
-}
